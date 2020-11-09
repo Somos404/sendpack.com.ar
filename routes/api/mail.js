eaaa061e4b12a48f72998dbf3b9fbfcb94bfe7eb
Bran <bbogdanich42@gmail.com>
2020-11-09 05:01:05 -0300

    mail

--- routes/api/mail.js
+++ routes/api/mail.js
@@ -1,76 +1,69 @@
 const router = require('express').Router()
 const { User } = require('../../db')
 const { check, validationResult } = require('express-validator')
-var nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const creds = require('../../config/config');
 const {checkToken} = require('../middlewares') 
+const { initial_address } = require('../../db')
 
-var transport = {
-    host: 'webmail.sendpack.com.ar',
-    port: 25,
-    auth: {
-        user: creds.USER,
-        pass: creds.PASS
-    }
-}
 
 router.use(function(req, res, next) {
   res.header(
     "Access-Control-Allow-Headers",
-    "user-token, Origin, Content-Type, Accept"
+    "user-token", "Origin", "Content-Type", "Accept"
   );
   next();
 });
 
+const smtpTransport = nodemailer.createTransport({
+	host: "webmail.misia.com.ar",
+	port: 465,
+  	secure: true, // use TLS
+	auth: {
+		user: 'misia@misia.com.ar',
+    	pass: 'Misia!2020'
+	},
+	tls: {
+		// do not fail on invalid certs
+		rejectUnauthorized: false
+	}
+});
 
-var transporter = nodemailer.createTransport(transport)
-
-//register users
-router.post('/send', [
-  checkToken
-], async (req, res, next) => {
-
-    const user = await User.findOne({
+//enviar mail  
+router.post('/send',[
+	checkToken
+], async (req, res) => {
+	
+	const user = await User.findOne({
       where: {
           id: req.usuarioId
       }
     })
-
-
-   /*  datosEnvio: datosEnvio,
-    tiempo: tiempo,
-    distancia: distancia,
-    costoEstimado: costoEstimado */
-
-    /* let datosEnvio = req.body.datosEnvio
-    let name = user.name
-    let para = user.email
-    let message = req.body.message
-    let content = `name: ${name} \n email: ${email} \n message: ${message} ` */
-
-    let content = `Hola!: ${user.name} ${user.last_name} \n este mail es por x motivo \n timepo: ${req.body.tiempo} \n distnacia: ${req.body.distancia} `
-    let mail = {
-      from: creds.USER,
-      to: user.email,  // Change to email address that you want to receive messages on
-      subject: 'cotizaciones sendpack',
-      text: content
-    }
-  
-    transporter.sendMail(mail, (err, data) => {
-      if (err) {
-
-        //grabo db el envio solicitado
-        res.status(200).send({
-          msg: 'se envio exitosamente',
-          ok:true
-		    });
-      } else {
-        res.status(200).send({
-          msg: 'no se pudo realizar el envio',
-          ok:false
-		  });
-      }
-    })
+	
+	let mailOptions = {
+	  from: 'sendpack@sendpack.com.ar',
+	  to: user.email,
+	  subject: 'Sendpack cotizacion',
+	  text: 'Have the most fun you can in a car. Get your Tesla today!'
+	};
+	
+	smtpTransport.sendMail(mailOptions, function (err) {
+	  if (err) {
+		res.status(200).send({
+			token: 'fallo al enviar email',
+			ok:false
+		});
+	  } else {
+		//grabar en db los datos del mail cuando envia el mail aunque puede tardar no se ejecuata  hasta que envia el mail puede pasar uno minutos para eso....
+		res.status(200).send({
+			token: 'email enviado',
+			ok:false
+		});
+	  }
+	});	
+	
+	// grabar en db y retornar msj api es inmediato sin importar si envia o no el mail
+    
 })
 
 
